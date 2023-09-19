de9d684c70228aeaeb2d04d4d6693909327b8241
CrispyyBaconx <immabaconback@gmail.com>
2023-09-19 01:43:56 -0400

    Added images and a bit of selection logic

--- js/popup.js
+++ js/popup.js
@@ -1,8 +1,16 @@
-// when the user edits something listen for it and save it to chrome.storage.local
 window.onload = function() {
-    // load the saved value from chrome.storage.local
+    const list = [
+        'sf.png',
+        'volcano.png',
+    ];
+
+    chrome.storage.local.get('excluded_items', function(result) {
+        if (!Array.isArray(result.excluded_items)) {
+            chrome.storage.local.set({ 'excluded_items': [] });
+        }
+    });
+
     chrome.storage.local.get('homepage_text', function(result) {
-        // if there is a saved value, update the input
         if (result.homepage_text) {
             document.getElementById('homepage_text').value = result.homepage_text;
         }
@@ -12,4 +20,38 @@ window.onload = function() {
     input.addEventListener('input', function() {
         chrome.storage.local.set({ 'homepage_text': input.value });
     });
+
+    // need to load images
+    var images = document.getElementById('images');
+
+    for (let i = 0; i < list.length; i++) {
+        var image = document.createElement('img');
+        var imageName = list[i];
+        image.src = 'assets/backgrounds/' + imageName;
+        image.id = imageName;
+        image.className = 'image';
+        image.addEventListener('click', function(id) {
+            return function() {
+                chrome.storage.local.get('excluded_items', function(result) {
+                    if (result.excluded_items) {
+                        if (result.excluded_items.includes(id)) {
+                            const updated_list = result.excluded_items.filter(item => item !== id);
+                            chrome.storage.local.set({ 'excluded_items': updated_list });
+                            let img = document.getElementById(id);
+                            img.className = 'image';
+                        } else {
+                            const updated_list = [...result.excluded_items, id];
+                            chrome.storage.local.set({ 'excluded_items': updated_list });
+                            let img = document.getElementById(id);
+                            img.className = 'image excluded';
+                        }
+                    } else {
+                        chrome.storage.local.set({ 'excluded_items': [id] });
+                        image.className = 'image excluded';
+                    }
+                });
+            };
+        }(imageName));
+        images.appendChild(image);
+    }
 }
--- js/tab_override.js
+++ js/tab_override.js
@@ -1,17 +1,26 @@
 window.onload = function() {
-    const files = [
+    const list = [
         'sf.png',
         'volcano.png',
-        // add more files here
-    ]
+    ];
+
+    chrome.storage.local.get('excluded_items', function(result) {
+        if(result.excluded_items) {
+            const temp_list = list.filter(item => !result.excluded_items.includes(item));
+            if(temp_list.length > 0) {
+                list = temp_list;
+            } else {
+                throw new Error('No images left to display');
+            }
+        }
+    });
 
     var image = document.getElementById('background');
-    var random = Math.floor(Math.random() * files.length);
-    // if(settings.backgroundRandom) {} maybe add this later if we want to add a setting for it
-    image.src = 'assets/backgrounds/' + files[random];
+    var random = Math.floor(Math.random() * list.length);
+
+    image.src = 'assets/backgrounds/' + list[random];
 
     var text = document.getElementById('text');
-
     chrome.storage.local.get('homepage_text', function(result) {
         // if there is a saved value, update the input
         if (result.homepage_text) {
@@ -21,6 +30,3 @@ window.onload = function() {
         }
     });
 }
-
-// add a thing for when the ext is clicked some settings for text and image selection
-// radio button array maybe with the images above and the dot below maybe
