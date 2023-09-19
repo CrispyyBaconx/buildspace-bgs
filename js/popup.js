a95744f2832b020d69ae14334b7e41b85a8027e5
CrispyyBaconx <immabaconback@gmail.com>
2023-09-19 00:15:44 -0400

    Added Persistent Storage + Text Option

--- js/popup.js
+++ js/popup.js
@@ -1,1 +1,15 @@
 // when the user edits something listen for it and save it to chrome.storage.local
+window.onload = function() {
+    // load the saved value from chrome.storage.local
+    chrome.storage.local.get('homepage_text', function(result) {
+        // if there is a saved value, update the input
+        if (result.homepage_text) {
+            document.getElementById('homepage_text').value = result.homepage_text;
+        }
+    });
+
+    var input = document.getElementById('homepage_text');
+    input.addEventListener('input', function() {
+        chrome.storage.local.set({ 'homepage_text': input.value });
+    });
+}
--- js/tab_override.js
+++ js/tab_override.js
@@ -10,9 +10,16 @@ window.onload = function() {
     // if(settings.backgroundRandom) {} maybe add this later if we want to add a setting for it
     image.src = 'assets/backgrounds/' + files[random];
 
-    // if(settings.text) {}
     var text = document.getElementById('text');
-    text.innerHTML = 'Welcome, Bacon!';
+
+    chrome.storage.local.get('homepage_text', function(result) {
+        // if there is a saved value, update the input
+        if (result.homepage_text) {
+            text.innerHTML = result.homepage_text;
+        } else {
+            text.innerHTML = 'Welcome, User!';
+        }
+    });
 }
 
 // add a thing for when the ext is clicked some settings for text and image selection
